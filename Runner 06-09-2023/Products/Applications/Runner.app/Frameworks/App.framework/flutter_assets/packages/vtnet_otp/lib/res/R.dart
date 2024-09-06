f2e2d0f14775e8f3d528e9af2f2880245002a316
Pham binh <binhpd.55cb@gmail.com>
2024-09-06 19:47:55 +0700

    add  archive file

---
+++ Runner 06-09-2023/Products/Applications/Runner.app/Frameworks/App.framework/flutter_assets/packages/vtnet_otp/lib/res/R.dart
@@ -0,0 +1,9 @@
+import 'package:vtnet_otp/res/generated/colors.dart';
+import 'package:vtnet_otp/res/generated/drawables.dart';
+
+class R {
+  R._();
+
+  static final Drawables drawable = Drawables();
+  static final Colors color = Colors();
+}
