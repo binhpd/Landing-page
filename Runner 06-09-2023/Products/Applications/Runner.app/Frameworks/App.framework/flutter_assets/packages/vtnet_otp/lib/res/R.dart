3108d98671c1fd8563c5800e7aa10fbb9b68d3ff
Pham binh <binhpd.55cb@gmail.com>
2024-11-21 11:10:16 +0700

    demo app store

--- Runner 06-09-2023/Products/Applications/Runner.app/Frameworks/App.framework/flutter_assets/packages/vtnet_otp/lib/res/R.dart
+++
@@ -1,9 +0,0 @@
-import 'package:vtnet_otp/res/generated/colors.dart';
-import 'package:vtnet_otp/res/generated/drawables.dart';
-
-class R {
-  R._();
-
-  static final Drawables drawable = Drawables();
-  static final Colors color = Colors();
-}
