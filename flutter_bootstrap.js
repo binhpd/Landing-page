690e1bd4378ab7e2c648e58a6da1086201ac852f
Phong_NguyenGia <36687028+phong010198@users.noreply.github.com>
2024-12-06 17:42:26 +0700

    Change design again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3921356377"
+    serviceWorkerVersion: "1743700545"
   }
 });
